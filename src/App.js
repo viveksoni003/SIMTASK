2e49b06ddae40f2929e8c83d66951507bdf7fcd7
Vivek Soni <vsoni2259@gmail.com>
2023-06-13 23:43:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import './App.scss';
+import Header from './components/Header';
+import HomeBanner from './components/HomeBanner';
+import Login from "./components/Login";
+import List from "./components/List";
+import Banner from "./components/Banner";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+         <Routes>
+          <Route path="/" element={
+            <React.Fragment>
+              <Header/>
+              <HomeBanner/>
+            </React.Fragment>
+          }/>
+          <Route path="/login" element={
+            <React.Fragment>
+              <Header/>
+              <Login/>
+              </React.Fragment>
+          }/>
+          <Route path="/register" element={
+            <React.Fragment>
+              <Header/>
+              <Login/>
+              </React.Fragment>
+          }/>
+          <Route path="/dashboard" element={
+            <React.Fragment>
+              <Header/>
+              <Banner/>
+              <List title="Netflix Originals" param="originals"/>
+              <List title="Trending Now" param="trending"/>
+              <List title="Now Playing" param="now_playing"/>
+              <List title="popular" param="popular"/>
+              <List title="Top Rated" param="top_rated"/>
+              <List title="Upcoming" param="upcoming"/>
+            </React.Fragment>
+          }/>   
+         </Routes>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6s5Ng-PENGVhXVmUUIELeR1jarJGO01c",
+  authDomain: "netflixclone-d8a24.firebaseapp.com",
+  projectId: "netflixclone-d8a24",
+  storageBucket: "netflixclone-d8a24.appspot.com",
+  messagingSenderId: "410305040973",
+  appId: "1:410305040973:web:3dc61b8b92b8cc46938204",
+  measurementId: "G-YE4NJGMLK5"
+}; 
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
